0c29633febb78abe4803da69b8f73917a58ceb50
arya <aryakhadakkar03@gmail.com>
2021-07-03 19:00:41 +0530

    SnakeAndladderProblems

---
+++ UC_1PlayerPosition_0.java
@@ -0,0 +1,13 @@
+package snakeAndLadderProblems;
+
+public class UC_1PlayerPosition_0 {
+	
+	public static void main(String[] args) {
+
+		int playerPosition = 0;
+		
+		System.out.println("playerPosition : " +playerPosition);
+		
+	}	
+
+}
